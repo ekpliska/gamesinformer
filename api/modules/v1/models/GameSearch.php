20e5c9e82c92f17cbf265a3fca12e67361959653
ekpliska <pliska.ek@gmail.com>
2020-05-16 14:24:42 +0300

    added: filter at the /api games

--- api/modules/v1/models/GameSearch.php
+++ api/modules/v1/models/GameSearch.php
@@ -1,17 +1,26 @@
 <?php
 
 namespace api\modules\v1\models;
+
 use yii\base\Model;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
 use yii\data\ActiveDataProvider;
 use api\modules\v1\models\Game;
+use common\models\Platform;
+use common\models\Genre;
 
 class GameSearch extends Game {
- 
+
+    public $platforms;
+    public $geners;
+
     public function rules() {
         return [
             [[
-                'title', 'series'
-            ], 'safe'],
+            'title', 'series',
+            'platforms', 'geners'
+                ], 'safe'],
         ];
     }
 
@@ -20,21 +29,21 @@ class GameSearch extends Game {
     }
 
     public function search($params) {
-        
-        $query = Game::find();
 
+        $platforms_ids = [];
+        $genres_ids = [];
+
+        $query = Game::find();
         $dataProvider = new ActiveDataProvider([
             'query' => $query
         ]);
-
         $this->load($params);
-
         if (!$this->validate()) {
             return $dataProvider;
         }
         
-        $query->where(['published' => true])->orderBy(['release_date' => SORT_ASC]);
-        
+        $query->where(['published' => true]);
+
         if (isset($params['title'])) {
             $query->andFilterWhere(['like', 'title', $params['title']]);
         }
@@ -42,7 +51,59 @@ class GameSearch extends Game {
         if (isset($params['series'])) {
             $query->andFilterWhere(['like', 'series', $params['series']]);
         }
-        
+
+        // Фильтр по жанрам
+        if (isset($params['genres'])) {
+            $genres_ids = $this->getGenresId(Html::encode($params['genres']));
+            $query->joinWith(['gameGenres' => function($q) use ($genres_ids) {
+                    $q->where(['in', 'genre_id', $genres_ids]);
+                }]);
+        }
+
+        // Фильтр по платформам
+        if (isset($params['platforms'])) {
+            $platforms_ids = $this->getPlatformsId(Html::encode($params['platforms']));
+            $query->joinWith(['gamePlatformReleases' => function($q) use ($platforms_ids) {
+                $q->where(['in', 'platform_id', $platforms_ids]);
+                // Если в фильтре используется одна платформа сортируем список публикаций по дате редиза игры
+                if (count($platforms_ids) == 1) {
+                    $q->orderBy(['date_platform_release' => SORT_DESC]);
+                }
+            }]);
+            // Если в фильтре используется более одной платформы сортируем список публикаций по дате публикации
+            if (count($platforms_ids) > 1) {
+                $query->orderBy(['publish_at' => SORT_DESC]);
+            }
+        } else {
+            $query->orderBy(['publish_at' => SORT_DESC]);
+        }
+
+
         return $dataProvider;
     }
+
+    private function getPlatformsId($params) {
+        $platforms = [];
+        $platforms_ids = [];
+        $platforms_names = [];
+        if (count($params) > 0) {
+            $platforms_names = explode(',', $params);
+            $platforms = Platform::find()->where(['IN', 'name_platform', $platforms_names])->asArray()->all();
+            $platforms_ids = ArrayHelper::getColumn($platforms, 'id');
+        }
+        return $platforms_ids;
+    }
+
+    private function getGenresId($params) {
+        $genres = [];
+        $genres_ids = [];
+        $genres_names = [];
+        if (count($params) > 0) {
+            $genres_names = explode(',', $params);
+            $genres = Genre::find()->where(['IN', 'name_genre', $genres_names])->asArray()->all();
+            $genres_ids = ArrayHelper::getColumn($genres, 'id');
+        }
+        return $genres_ids;
+    }
+
 }
