65e2681b949e8135fc19ecc8cc0dfd66d4640504
ekpliska <pliska.ek@gmail.com>
2020-05-25 08:31:20 +0300

    added: test controller

---
+++ frontend/controllers/TestController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace frontend\controllers;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
+use common\models\TokenPushMobile;
+use common\components\firebasePush\FirebaseNotifications;
+
+/**
+ * Site controller
+ */
+class TestController extends Controller {
+
+    public function behaviors() {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['index', 'update'],
+                'rules' => [
+                    [
+                        'actions' => ['daily', 'push'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Игры, сохранение
+     */
+    public function actionDaily() {
+        $_tokens = TokenPushMobile::find()->andWhere(['enabled' => true])->asArray()->all();
+        $tokens = ArrayHelper::getColumn($_tokens, 'token');
+        $notes = new FirebaseNotifications();
+        $result = $notes->sendNotification($tokens, null, ['daily_games_count' => 7]);
+        echo '<pre>';
+        var_dump($result);
+    }
+
+    /*
+     * Игры, редактирование
+     */
+
+    public function actionPush() {
+        $result = TokenPushMobile::send('Состоялся релиз новой игры', 'Название игры');
+        echo '<pre>';
+        var_dump($result);
+    }
+    
+}
