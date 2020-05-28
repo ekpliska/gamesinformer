6c9971c748e4d6bf11ea9ef4c5a8dfb5e7d5e354
ekpliska <pliska.ek@gmail.com>
2020-05-28 13:06:17 +0300

    fixed: device token

--- common/models/TokenPushMobile.php
+++ common/models/TokenPushMobile.php
@@ -43,6 +43,9 @@ class TokenPushMobile extends ActiveRecord {
             $new_token->token = $_token;
             $new_token->is_auth = $user_id ? 1 : 0;
             return $new_token->save(false) ? true : false;
+        } elseif ($push_token && $user_id) {
+            $push_token->is_auth = 1;
+            return $push_token->save(false) ? true : false;
         }
         
         return false;
