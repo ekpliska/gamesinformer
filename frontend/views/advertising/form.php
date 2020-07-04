79776270c6dd18dfd54b9ff654695a029a0ce2b3
ekpliska <pliska.ek@gmail.com>
2020-07-04 15:56:19 +0300

    fixed: advertising block

--- frontend/views/advertising/form.php
+++ frontend/views/advertising/form.php
@@ -20,29 +20,37 @@ use yii\widgets\ActiveForm;
 ?>
 <div class="modal-body">
     <div class="row">
-    <div class="col-lg-12">
-        <?= $form->field($model, 'title')->input('text')->label() ?>
-    </div>
-    <div class="col-lg-12">
-        <?= $form->field($model, 'description')->textarea(['rows' => 5])->label() ?>
-    </div>
-    <div class="col-lg-12">
-        <?= $form->field($model, 'youtube')->input('text')->label() ?>
-    </div>
-    <div class="col-lg-7">
-        <?= $form->field($model, 'is_preview_youtube')->checkbox()->label(false) ?>
-        <?= $form->field($model, 'image')->input('file')->label() ?>
-    </div>
-    <div class="col-lg-5">
-        <?php if (!empty($model->preview)) : ?>
-            <div class="advertising-preview">
-                <img 
-                    src="<?= 'http://api.gamenotificator.net' . $model->preview ?>" 
-                    class="img-thumbnail" alt="preview" 
-                    onerror="this.onerror=null;this.src='<?= $model->preview ?>';">
+        <div class="col-lg-12">
+            <?= $form->field($model, 'title')->input('text')->label() ?>
+        </div>
+        <div class="col-lg-12">
+            <?= $form->field($model, 'description')->textarea(['rows' => 2])->label() ?>
+        </div>
+        <div class="col-lg-12">
+            <?= $form->field($model, 'youtube')->input('text')->label() ?>
+        </div>
+        <div class="col-lg-7">
+            <?= $form->field($model, 'is_preview_youtube')->checkbox()->label(false) ?>
+            <?= $form->field($model, 'image')->input('file')->label() ?>
+        </div>
+        <div class="col-lg-5">
+            <?php if (!empty($model->preview)) : ?>
+                <div class="advertising-preview">
+                    <img 
+                        src="<?= 'http://api.gamenotificator.net' . $model->preview ?>" 
+                        class="img-thumbnail" alt="preview" 
+                        onerror="this.onerror=null;this.src='<?= $model->preview ?>';">
+                </div>
+            <?php endif; ?>
+        </div>
+        <div class="col-lg-12">
+            <div class="alert alert-warning">
+                <strong>Внимание!</strong>
+                Для превью из YouTube ролика обязательно указывайте полную ссылку 
+                содержащую <code>watch?v=<КОД_ВИДЕО></code>. 
+                Например: <code>https://www.youtube.com/watch?v=BODEmT7OKWg</code>
             </div>
-        <?php endif; ?>
-    </div>
+        </div>
     </div>
 </div>
 
