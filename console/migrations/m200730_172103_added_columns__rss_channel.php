c3173f3f549c4aedb4eab13a2b28711954d499a8
pliska_ek <ekaterina.pliska@am-intech.ru>
2020-07-31 16:34:14 +0300

    added: insert records at the rss migration

--- console/migrations/m200730_172103_added_columns__rss_channel.php
+++ console/migrations/m200730_172103_added_columns__rss_channel.php
@@ -20,6 +20,51 @@ class m200730_172103_added_columns__rss_channel extends Migration {
 
         $this->update('{{%rss_channel}}', ['type' => RssChannel::TYPE_NEWS]);
 
+        $this->batchInsert('{{%rss_channel}}', ['rss_channel_name', 'rss_channel_url', 'channel_id', 'site_url', 'type'], [
+            [
+                'PlayGround.ru',
+                'https://www.youtube.com/feeds/videos.xml?channel_id=UCN1U8E6ClQVC7y6N9HpMm9Q',
+                'UCN1U8E6ClQVC7y6N9HpMm9Q',
+                'https://www.youtube.com/channel/UCN1U8E6ClQVC7y6N9HpMm9Q',
+                RssChannel::TYPE_YOUTUBE,
+            ],
+            [
+                'Игромания',
+                'http://www.youtube.com/feeds/videos.xml?channel_id=UC_Q1vhf7wcR_zGlc5ahAg0A',
+                'UC_Q1vhf7wcR_zGlc5ahAg0A',
+                'https://www.youtube.com/channel/UCN1U8E6ClQVC7y6N9HpMm9Q',
+                RssChannel::TYPE_YOUTUBE,
+            ],
+            [
+                'Навигатор игрового мира',
+                'http://www.youtube.com/feeds/videos.xml?channel_id=UCfDPpLHN-7_6cctaU8CgPhQ',
+                'UCfDPpLHN-7_6cctaU8CgPhQ',
+                'https://www.youtube.com/channel/UCfDPpLHN-7_6cctaU8CgPhQ',
+                RssChannel::TYPE_YOUTUBE,
+            ],
+            [
+                'StopGame.Ru',
+                'http://www.youtube.com/feeds/videos.xml?channel_id=UCq7JZ8ATgQWeu6sDM1czjhg',
+                'UCq7JZ8ATgQWeu6sDM1czjhg',
+                'https://www.youtube.com/channel/UCq7JZ8ATgQWeu6sDM1czjhg',
+                RssChannel::TYPE_YOUTUBE,
+            ],
+            [
+                'GohaMedia',
+                'http://www.youtube.com/feeds/videos.xml?channel_id=UCww7wZ5uMaN5zIeq3srIgjA',
+                'UCww7wZ5uMaN5zIeq3srIgjA',
+                'https://www.youtube.com/channel/UCww7wZ5uMaN5zIeq3srIgjA',
+                RssChannel::TYPE_YOUTUBE,
+            ],
+            [
+                'MMORPG.SU. Онлайн игры',
+                'http://www.youtube.com/feeds/videos.xml?channel_id=UCxxdTg_nqFdADGrz2eJ6hGA',
+                'UCxxdTg_nqFdADGrz2eJ6hGA',
+                'https://www.youtube.com/channel/UCxxdTg_nqFdADGrz2eJ6hGA',
+                RssChannel::TYPE_YOUTUBE,
+            ],
+        ]);
+
     }
 
     public function safeDown() {
