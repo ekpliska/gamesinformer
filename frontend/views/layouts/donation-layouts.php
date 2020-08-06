d413f6b4af7fd6cce4f223ed1229fdbc8b7f0c07
pliska_ek <ekaterina.pliska@am-intech.ru>
2020-08-06 14:35:45 +0300

    fixed: donation page, css

--- frontend/views/layouts/donation-layouts.php
+++ frontend/views/layouts/donation-layouts.php
@@ -18,12 +18,35 @@ AppAsset::register($this);
     <?php $this->registerCsrfMetaTags() ?>
     <link rel="icon" href="/favicon.png" sizes="32x32" type="image/png">
     <title><?= Html::encode($this->title) ?></title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
     <?php $this->head() ?>
+</head>
 </head>
 <body>
 <?php $this->beginBody() ?>
     <div class="donation-page">
+        <h4>
+            Если Вам нравится наш проект и Вы хотите побыстрее увидеть что еще мы подготовили для Вас, то можете рассказать о нас своим друзьям или поддержать донатом!
+        </h4>
         <?= $content ?>
+        <div class="donation-page__footer">
+            <div>
+                <?= Html::img('@web/images/logo.png', ['alt' => 'logo']) ?>
+            </div>
+            <div>
+                <p>&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+                <p>
+                    <a href="mailto:inbox@gamenotificator.net">mailto:inbox@gamenotificator.net</a>
+                </p>
+            </div>
+            <div>
+                <p>
+                    <a class="btn btn-vk" href="https://vk.com/gamenotificator" target="_blank">
+                        <i class="fab fa-vk"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
     </div>
 <?php $this->endBody() ?>
 </body>
