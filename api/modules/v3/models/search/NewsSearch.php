8bd0f4c7729633be5d6e28ca24b0289fb5358f55
pliska_ek <ekaterina.pliska@am-intech.ru>
2020-08-10 16:27:48 +0300

    added: GET type param at the news api

--- api/modules/v3/models/search/NewsSearch.php
+++ api/modules/v3/models/search/NewsSearch.php
@@ -13,6 +13,12 @@ class NewsSearch extends News {
     public $title;
     public $rss_name;
     public $type;
+    public $type_list;
+
+    public function __construct($config = array()) {
+        $this->type_list = RssChannel::getTypesListApi();
+        parent::__construct($config);
+    }
 
     public function rules() {
         return [
@@ -27,7 +33,6 @@ class NewsSearch extends News {
     public function search($params) {
         
         $rss_ids = [];
-        $types = RssChannel::getTypesListApi();
         $type = isset($params['type']) ? $params['type'] : null;
 
         $query = News::find();
@@ -42,27 +47,31 @@ class NewsSearch extends News {
         
         $query->orderBy(['pub_date' => SORT_DESC]);
 
-        if (in_array($type, $types)) {
-            $query->joinWith('rss')->where(['type' => array_search($type, $types)]);
+        if (in_array($type, $this->type_list)) {
+            $query->joinWith('rss')->where(['type' => array_search($type, $this->type_list)]);
         } else {
             return $dataProvider;
         }
 
         if (isset($params['rss_ids'])) {
-            $rss_ids = $this->checkRssIds($params['rss_ids']);
+            $rss_ids = $this->checkRssIds($params['rss_ids'], $type);
             $query->where(['in', 'rss_channel_id', $rss_ids]);
         }
         
         return $dataProvider;
     }
     
-    private function checkRssIds($params) {
+    private function checkRssIds($params, $type) {
         $rss = [];
         $rss_ids = [];
         $rss_names = [];
         if (count($params) > 0) {
             $ids_arr = explode(',', $params);
-            $rss = RssChannel::find()->where(['IN', 'id', $ids_arr])->asArray()->all();
+            $rss = RssChannel::find()
+                ->where(['IN', 'id', $ids_arr])
+                ->andWhere(['type' => array_search($type, $this->type_list)])
+                ->asArray()
+                ->all();
             $rss_ids = ArrayHelper::getColumn($rss, 'id');
         }
         return $rss_ids;
