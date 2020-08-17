d05e05c4fccf410bea32425b1bddca46417b2308
pliska_ek <ekaterina.pliska@am-intech.ru>
2020-08-17 11:30:05 +0300

    fixed:

--- frontend/views/donation/index.php
+++ frontend/views/donation/index.php
@@ -37,8 +37,8 @@
         Если Вам нравится наш проект и Вы хотите побыстрее увидеть что еще мы подготовили для Вас, то можете рассказать о нас своим друзьям или поддержать донатом!
     </h4>
     <?php
-        $mrh_login = "videogames";
-        $mrh_pass1 = "lQ3K4Jd62IacyCCCWq0V";
+        $mrh_login = $_ENV['mrh_login'];
+        $mrh_pass1 = $_ENV['mrh_pass1'];
         $inv_id = 0;
         $inv_desc = "Пожертвование проекту GamePlay";
         $def_sum = "100";
