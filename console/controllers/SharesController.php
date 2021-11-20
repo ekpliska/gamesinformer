79793b37c6125b6b166f174205416aaa835f0d6c
ekpliska <pliska.ek@gmail.com>
2021-11-21 00:24:42 +0300

    fixed: shares console

--- console/controllers/SharesController.php
+++ console/controllers/SharesController.php
@@ -13,74 +13,46 @@ class SharesController extends Controller
 
     /**
      * Рассылка уведомлений о старте раздач
-     * Ежедневно каждый час
-     */
-    public function actionSend()
-    {
-        $current_date = new \DateTime('NOW', new \DateTimeZone('Europe/Moscow'));
-        $shares = Shares::find()->all();
-        $games_list = [];
-
-        if (count($shares) == 0) {
-            return false;
-        }
-
-        $count_new_shares = 0;
-
-        foreach ($shares as $key => $item) {
-            $date_start = new \DateTime($item->date_start, new \DateTimeZone('Europe/Moscow'));
-            $diff = $current_date->diff($date_start);
-            $hours = $diff->h + ($diff->days * 24);
-            if ($hours == 0) {
-                $games_list[] = $item->game_list;
-                $count_new_shares++;
-            }
-        }
-
-        if ($count_new_shares > 0) {
-            $notification = new Notifications(
-                Notifications::SHARES_TYPE,
-                null, null,
-                ['games_list' => implode(', ', $games_list)]
-            );
-            $notification->createNotification();
-        }
-    }
-
-    /**
      * Проверка даты начала у Скидок/Акций/Раздач
      * Проверка даты окончания у Скидок/Акций/Раздач
-     * Ежедневно в 20:00
+     * Ежедневно каждый час
      */
     public function actionCheckDateEnd()
     {
-        $date = new \DateTime('NOW', new \DateTimeZone('Europe/Moscow'));
-        $current_date = strtotime($date->format('Y-m-d 00:00:00'));
+        $current_date = new \DateTime('NOW', new \DateTimeZone('Europe/Moscow'));
 
         $shares = Shares::find()->all();
         if (count($shares)) {
             foreach ($shares as $key => $share) {
-                $end = new \DateTime($share->date_end);
-                $date_end = strtotime($end->format('Y-m-d 00:00:00'));
+                $date_end = new \DateTime($share->date_end);
+                $date_start = new \DateTime($share->date_start);
 
-                $start = new \DateTime($share->date_start);
-                $date_start = strtotime($start->format('Y-m-d 00:00:00'));
-
-                if (($current_date === $date_start) ||
-                    ($current_date > $date_start && $current_date < $date_end) &&
+                if (($current_date->diff($date_start)->days === 0) &&
+                    ($current_date->diff($date_start)->h === 0) &&
+                    ($current_date->diff($date_start)->i === 0) &&
                     !$share->is_published
                 ) {
                     $share->is_published = 1;
                     $share->save();
+
+                    $games_list[] = $share->game_list;
+
+                    $notification = new Notifications(
+                        Notifications::SHARES_TYPE,
+                        null, null,
+                        ['games_list' => implode(', ', $games_list)]
+                    );
+                    $notification->createNotification();
+
                     continue;
-                } else if (($current_date === $date_end) ||
-                    ($current_date > $date_start && $current_date > $date_end) &
+                } else if (($current_date->diff($date_end)->days === 0) &&
+                    ($current_date->diff($date_end)->h === 0) &&
+                    ($current_date->diff($date_end)->i === 0) &&
                     $share->is_published
                 ) {
                     $share->is_published = 0;
                     $share->save();
-                } else {
-
+                    continue;
                 }
             }
         }
